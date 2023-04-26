41cc55de27013543ba509d4874aecff2604e726c
Erfan Momeni <Erfan.Momeni@snapp.cab>
2023-04-26 19:08:30 +0430

    fix: fix problem

--- run.py
+++ run.py
@@ -1,4 +1,26 @@
+import argparse
 from app.app import app
 
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Translator Service"
+    )
+    parser.add_argument(
+        "--host",
+        type=str,
+        help="Hostname (%(default)s)",
+        default="127.0.0.1"
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        help="Port (%(default)s)",
+        default=5000)
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    args = get_args()
+    app.run(host=args.host, port=args.port, debug=True)
